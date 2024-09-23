1cea378244f41b91397846cda8005c8b1a3c755f
Melissa <125832670+malvarez12@users.noreply.github.com>
2024-09-22 20:42:12 -0600

    db

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,37 +1,39 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
+            $table->id();
+            $table->uuid();
+            $table->string('username')->nullable();
             $table->string('name');
             $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->integer('branch_id')->default(1);
-            $table->integer('role_id')->default(1);
+            $table->string("store_name")->nullable();
+            $table->string("store_address")->nullable();
+            $table->string("store_phone")->nullable();
+            $table->string("store_email")->nullable();
             $table->rememberToken();
             $table->timestamps();
+            $table->string('photo')->nullable();
         });
     }
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('users');
     }
-}
+};
